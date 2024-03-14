482b9b9bc4c8e492613c444fc6475d6477bd1892
Joshua Sing <joshua@bloq.com>
2024-03-15 03:42:44 +1100

    Use 'errors.Is' to compare errors

--- service/bfg/bfg.go
+++ service/bfg/bfg.go
@@ -1461,7 +1461,7 @@ func (s *Server) Run(pctx context.Context) error {
 		s.wg.Add(1)
 		go func() {
 			defer s.wg.Done()
-			if err := d.Run(ctx, cs); err != context.Canceled {
+			if err := d.Run(ctx, cs); !errors.Is(err, context.Canceled) {
 				log.Errorf("prometheus terminated with error: %v", err)
 				return
 			}
